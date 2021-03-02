4eb4a96838cf9c31ac2f391ed5fdc5680b106ca0
Natan Nobre <natannobre37@gmail.com>
2021-03-01 22:39:53 -0300

    Updated

--- NodeJS/serverHTTP.js
+++ NodeJS/serverHTTP.js
@@ -1,11 +1,73 @@
 var http = require('http');
 var express = require('express')
 var app = express()
+const bodyParser = require('body-parser')
+const fs = require('fs');
+const pathUsers = "listUsers.txt"
+const pathTasks = "listTasks.txt"
+
+var listTasks = []
+
+var listUsers = [{
+                    id:1, 
+                    name:"ruan",
+                    login:"felipe@alu.ufc"
+                },
+                {
+                    id:2, 
+                    name:"alan",
+                    login:"alan@alu.ufc"
+                },
+                {
+                    id:3, 
+                    name:"isaac",
+                    login:"isaac@alu.ufc"
+                },
+                {
+                    id:4, 
+                    name:"natan",
+                    login:"natan@alu.ufc"
+                }]
+
+try
+{
+    var readUsers = fs.readFileSync(pathUsers, 'utf8')
+    if (!readUsers)
+    {
+        fs.writeFileSync(pathUsers, JSON.stringify(listUsers))
+    }            
+    else
+    {
+        listUsers = JSON.parse(readUsers)
+    }
+}
+catch(error)
+{
+    fs.writeFileSync(path, JSON.stringify(listUsers))
+}
+
+try
+{
+    
+    var readTasks = fs.readFileSync(pathTasks, 'utf8')
+    if (!readTasks)
+    {
+        fs.writeFileSync(pathTasks, JSON.stringify(listTasks))
+    }
+    else
+    {
+        listTasks = JSON.parse(readTasks)
+    }
+}
+catch(error)
+{
+    fs.writeFileSync(pathTasks, JSON.stringify(listTasks))
+}
+    
 
 //Servidor
 var port = 9999
 var hostname = "localhost"
-const bodyParser = require('body-parser')
 
 app.use(
     bodyParser.urlencoded({ extended: true })
@@ -13,20 +75,43 @@ app.use(
 
 app.use(bodyParser.json())
 
-app.get('/salvarLogin', (req, res) => {
-
+app.get('/users', (req, res) => {
+    res.json(listUsers)
 })
 
-app.get('/get2', (req, res) => {
-
+app.get('/users/:id', (req, res) => {
+    console.log(req.params.id)
+    var user = listUsers.find(user => req.params.id == user.id)
+    res.json(user)
 })
 
-app.post('/post1', (req, res) => {
-    console.log(req.body.data);
+app.get('/tasks', (req, res) => {
+    res.json(listTasks)
 })
 
-app.post('/post2', (req, res) => {
-    console.log(req.body.data)
+app.post('/saveLogin', (req, res) => {
+    var user = {
+        id      : listUsers.length + 1,
+        name    : req.body.name,
+        login   : req.body.login
+    }
+    console.log(req.body.login, req.body.name)
+    listUsers.push(user)
+    fs.writeFileSync('listUsers.txt', JSON.stringify(listUsers))
+    // fs.appendFile('listUsers.txt', JSON.stringify(user), error => {if (error) console.log(error)})
+    res.json(user)
+})
+
+app.post('/tasks', (req, res) => {
+    var task = {
+        id      : listTasks.length + 1,
+        name    : req.body.name,
+        done    : false
+    }
+    console.log(req.body.name)
+    listTasks.push(task)
+    fs.writeFileSync('listTasks.txt', JSON.stringify(listTasks))
+    res.json(task)
 })
 
 app.listen(port, hostname, () => {
